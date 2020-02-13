58c077c21e773b1007fa4b39c560185986a4b005
Taffarel Xavier <taffarel_deus@hotmail.com>
2020-02-13 12:53:13 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [user, setUsers] = useState([]);
+
+    useEffect(() => {
+        function buscarDados() {
+            fetch('http://api.negaodocinda.com.br/users')
+                .then(response => {
+                    return response.json();
+                })
+                .then(result => {
+                    setUsers(result);
+                });
+        }
+
+        buscarDados();
+    }, []);
+
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <h1>Cadastros realizados</h1>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <table border="" className="table" style={{ width: '100%', borderWidth: 1, borderColor: '#fff' }}>
+                        <thead>
+                            {user.length > 0 ? (
+                                <tr>
+                                    <th>NOME</th>
+                                    <th>EMAIL</th>
+                                    <th>WHATSAPP</th>
+                                </tr>
+                            ) : (
+                                <tr>
+                                    <th>NOME</th>
+                                    <th>EMAIL</th>
+                                    <th>WHATSAPP</th>
+                                </tr>
+                            )}
+                        </thead>
+
+                        <tbody>
+                            {user.length > 0 ? (
+                                <>
+                                    {user.map(({ id, username, email, whatsapp }, index) => {
+                                        return (
+                                            <tr key={index}>
+                                                <td>{username}</td>
+                                                <td>{email}</td>
+                                                <td>{whatsapp}</td>
+                                            </tr>
+                                        );
+                                    })}
+                                </>
+                            ) : (
+                                <tr>
+                                    <td>...</td>
+                                    <td>...</td>
+                                    <td>...</td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default App;
